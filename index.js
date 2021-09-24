060c6796afacbc2bab10b2f0153e5e5a08dd51c3
azamiset <azamiset@google.com>
2021-09-24 22:12:51 +0700

    connect to api   modified:   home.html 	modified:   index.js 	new file:   package-lock.json 	modified:   package.json

--- index.js
+++ index.js
@@ -3,16 +3,37 @@ const fs = require('fs');
 const requests = require('requests');
 
 const homeFile = fs.readFileSync("home.html", "utf-8");
-http.createServer((req, res) => {
-  if (req.url === "/") {
-    requests('https://google.com/foo/bar', { streaming })
-      .on('data', function (chunk) {
-        console.log(chunk);
-      })
-      .on('end', function (err) {
-        if (err) return console.log('connection closed due to errors', err);
 
-        console.log('end');
+const replaceVal = (tempVal, orgVal) => {
+  let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+  temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+  temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+  temperature = temperature.replace("{%location%}", orgVal.name);
+  temperature = temperature.replace("{%country%}", orgVal.sys.country);
+  temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+  return temperature;
+}
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    requests('https://api.openweathermap.org/data/2.5/weather?q=sukabumi&appid=eb898ea28cf3715b4d2af756170dbd21')
+      .on('data', (chunk) => {
+        const objData = JSON.parse(chunk)
+        const arrData = [objData];
+        // console.log(arrData[0].main.temp);
+        const realTimeData = arrData
+          .map(val => replaceVal(homeFile, val))
+          .join("");
+        res.write(realTimeData);
+        // console.log(realTimeData);
+      })
+      .on('end', (err) => {
+        if (err) return console.log('connection closed due to errors', err);
+        res.end();
       });
   }
+});
+
+server.listen(3000, "127.0.0.1", () => {
+  console.log('server running');
 })
